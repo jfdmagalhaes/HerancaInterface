453c185a1eec7687bf6fd3ab53d259a876f520fc
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-12-17 18:09:51 -0300

    Iniciando heranças e interfaces

---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+
+    }
+}
